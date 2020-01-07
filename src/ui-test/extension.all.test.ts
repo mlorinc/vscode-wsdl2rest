4878677f86096ebe5765c945a6cb59f686f1e0b9
Marian Lorinc <mlorinc@redhat.com>
2020-01-07 18:25:26 +0100

    Add extension ui test
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

---
+++ src/ui-test/extension.all.test.ts
@@ -0,0 +1,87 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as marketplaceTest from './marketplace.test';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as assert from 'assert';
+import * as extensionTest from './extension.test';
+import * as webserver from '../test/app_soap';
+import { expect } from 'chai';
+import { Project } from 'vscode-uitests-tooling';
+import { VSBrowser, WebDriver } from 'vscode-extension-tester';
+
+describe('All tests', function () {
+	marketplaceTest.test();
+
+	describe('Extension tests', function() {
+		let browser: VSBrowser;
+		let driver: WebDriver;
+		let workspace: Project;
+
+		before('Setup environment', async function() {
+			this.timeout(23000);
+			browser = VSBrowser.instance;
+			driver = browser.driver;
+
+			workspace = await prepareWorkspace();
+			webserver.startWebService();
+		});
+
+		after('Clear environment', async function() {
+			await clearWorkspace(workspace);
+			webserver.stopWebService();
+		});
+
+		for (const f of walk(path.join(process.cwd(), 'src/ui-test/test-data'))) {
+			assert(f.endsWith('.json'), `${f} is not json file`);
+			const fileContent = fs.readFileSync(f, { encoding: 'utf8' });
+			extensionTest.test(JSON.parse(fileContent));
+		}
+	});
+
+});
+
+function* walk(dir: string): Iterable<string> {
+	const stack = [dir];
+
+	while (stack.length > 0) {
+		const file = stack.pop();
+		const stat = fs.statSync(file);
+
+		if (stat && stat.isDirectory()) {
+			stack.push(...fs.readdirSync(file).map(f => path.join(file, f)));
+		} else {
+			yield file;
+		}
+	}
+}
+
+async function prepareWorkspace(): Promise<Project> {
+	const project = new Project(extensionTest.WORKSPACE_PATH);
+	expect(project.exists).to.be.false;
+	project.create();
+	expect(project.exists).to.be.true;
+	await project.open();
+	return project;
+}
+
+
+async function clearWorkspace(workspace: Project): Promise<void> {
+	await workspace.close();
+	await workspace.delete();
+}
---
+++ src/ui-test/extension.test.ts
@@ -0,0 +1,342 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from 'fs';
+import * as fsExtra from 'fs-extra';
+import * as path from 'path';
+import { Command, getPackageData, PackageData } from './package_data';
+import {
+	CommandPalette,
+	DefaultWait,
+	Dialog,
+	Input,
+	LogAnalyzer,
+	Maven,
+	OutputViewExt
+} from 'vscode-uitests-tooling';
+import { expect } from 'chai';
+import {
+	InputBox,
+	VSBrowser,
+	WebDriver,
+	Workbench
+} from 'vscode-extension-tester';
+
+type Runtime = 'spring' | 'blueprint';
+type GenerationType = 'url' | 'file';
+
+const mavenGoals = {
+	spring: 'camel:run',
+	blueprint: 'camel:run'
+};
+
+export interface TestArguments {
+	camelVersion: string;
+	framework: Runtime;
+	type: GenerationType;
+}
+
+interface RuntimeOutput {
+	totalRoutes: string;
+	startedRoutes: string;
+	camelVersion: string;
+}
+
+const RUNTIME_FOLDER = path.join(process.cwd(), 'src', 'ui-test', 'runtimes');
+const WSDL_FILE = path.join(process.cwd(), 'src', 'test', 'address.wsdl');
+const WSDL_URL = 'http://localhost:3000/helloworldservice?wsdl';
+export const WORKSPACE_PATH = path.join(process.cwd(), '.ui-testing');
+
+export function test(args: TestArguments) {
+	describe(`Extension test[${detailsString(args)}]`, function () {
+		let browser: VSBrowser;
+		let driver: WebDriver;
+		let packageData: PackageData = getPackageData();
+
+		before('Project setup', async function () {
+			browser = VSBrowser.instance;
+			driver = browser.driver;
+			fsExtra.copySync(path.join(RUNTIME_FOLDER, args.framework), WORKSPACE_PATH)
+		});
+
+		after('Project cleanup', async function () {
+			for (const f of fs.readdirSync(WORKSPACE_PATH)) {
+				fsExtra.removeSync(path.join(WORKSPACE_PATH, f));
+			}
+		});
+
+		const command: Command = findCommand(args, packageData);
+
+		it(`Execute command: ${command.command}`, async function () {
+			this.timeout(4000);
+			const cmd = await CommandPalette.open();
+			await cmd.executeCommand(command.title);
+		});
+
+		it(`Open wsdl file [${args.type}]`, async function () {
+			this.timeout(20000);
+			switch (args.type) {
+				case 'url':
+					const input = await getInput();
+					input.test({
+						placeholder: 'Provide the URL for the WSDL file',
+						message: 'WSDL URL (Press \'Enter\' to confirm or \'Escape\' to cancel)'
+					});
+
+					await input.setText(WSDL_URL);
+					await input.confirm();
+					break;
+				case 'file':
+					await Dialog.confirm(WSDL_FILE);
+					break;
+			}
+		});
+
+		it(`Select '${args.framework}' option`, async function () {
+			const input = await getInput();
+
+			await input.test({
+				placeholder: 'Specify which DSL to generate the Camel configuration for',
+				quickPicks: ['Spring', 'Blueprint']
+			});
+
+			await input.setText(args.framework);
+			await input.confirm();
+		});
+
+		it(`Confirm output directory`, async function () {
+			const input = await getInput();
+
+			await input.test({
+				placeholder: 'Enter the output directory for generated artifacts',
+				message: 'Output Directory (Press \'Enter\' to confirm or \'Escape\' to cancel)',
+				text: 'src/main/java'
+			});
+
+			await input.confirm();
+		});
+
+		it('Confirm JAX-WS endpoint', async function () {
+			const input = await getInput();
+
+			await input.test({
+				placeholder: 'Enter the address for the running jaxws endpoint (defaults to http://localhost:8080/somepath)',
+				message: 'JAXWS Endpoint (Press \'Enter\' to confirm or \'Escape\' to cancel)'
+			});
+
+			await input.confirm();
+		});
+
+		it('Confirm JAX-RS endpoint', async function () {
+			const input = await getInput();
+
+			await input.test({
+				placeholder: 'Enter the address for the jaxrs endpoint (defaults to http://localhost:8081/jaxrs)',
+				message: 'JAXRS Endpoint (Press \'Enter\' to confirm or \'Escape\' to cancel)'
+			});
+
+			await input.setText('http://localhost:8000/jaxrs');
+			await input.confirm();
+		});
+
+		it('Convert wsdl project', async function () {
+			this.timeout(9000);
+			const output = await OutputViewExt.open();
+			const hasText = await output.waitUntilContainsText('Process finished. Return code 0.', 6000);
+			expect(hasText).to.be.true;
+		});
+
+		describe('Generated all files', function () {
+			const files = getExpectedFileList(args);
+
+			for (const file of files) {
+				it(`Generated ${file}`, async function () {
+					this.retries(5);
+					const exists = fs.existsSync(path.join(WORKSPACE_PATH, file));
+					expect(exists).to.be.true;
+
+					if (!exists) {
+						await DefaultWait.sleep(150);
+					}
+				});
+			}
+
+			it('Show notifications', async function () {
+				const notifications = await new Workbench().getNotifications();
+				let notification = notifications.find(async n => await n.getMessage() == `Created ${getCamelContextPath(args)}`);
+
+				if (notification === undefined) {
+					expect.fail('Did not find notification');
+				}
+
+				notification = notifications.find(async n => await n.getMessage() == 'Created CXF artifacts for specified WSDL at src/main');
+			});
+		});
+
+		describe('Test generated project', function () {
+			let maven: Maven;
+
+			before('Install maven project', async function () {
+				this.timeout(0);
+				const exitCode = await prepareMavenProject(args.framework, args.camelVersion);
+				expect(exitCode).to.equal(0);
+			});
+
+			after('Make sure maven is not running', async function () {
+				if (maven.isRunning) {
+					await maven.exit();
+				}
+			});
+
+			it('Run projects', async function () {
+				this.timeout(10000);
+				maven = executeProject(args.framework, args.camelVersion);
+				const data = await analyzeProject(maven);
+				const expectedRoutesCount = getExpectedNumberOfRoutes(args);
+
+				expect(parseInt(data.startedRoutes), "All routes were not started").to.equal(expectedRoutesCount);
+				expect(parseInt(data.totalRoutes), "Number of routes does not match").to.equal(expectedRoutesCount);
+				expect(data.camelVersion, "Camel version mismatch").to.equal(args.camelVersion);
+			});
+		});
+
+	});
+}
+
+function findCommand(args: TestArguments, packageData: PackageData): Command {
+	switch (args.type) {
+		case 'url':
+			return packageData.contributes.commands.find(x => x.command.endsWith('url'));
+		case 'file':
+			return packageData.contributes.commands.find(x => x.command.endsWith('local'));
+	}
+}
+
+function detailsString(args: TestArguments): string {
+	let segments: string[] = [];
+
+	switch (args.type) {
+		case 'url':
+			segments.push(`url = ${WSDL_URL}`);
+		case 'file':
+			segments.push(`file = ${WSDL_FILE}`);
+	}
+	segments.push(args.framework);
+	segments.push(`camel = ${args.camelVersion}`);
+	return segments.join(', ');
+}
+
+async function prepareMavenProject(runtime: Runtime, camelVersion: string): Promise<number> {
+	const maven = new Maven({
+		args: ['clean', 'install'],
+		properties: {
+			'camel.version': camelVersion
+		},
+		cwd: WORKSPACE_PATH
+	});
+	maven.spawn();
+	return maven.wait();
+}
+
+function executeProject(runtime: Runtime, camelVersion: string): Maven {
+	const maven = new Maven({
+		args: [mavenGoals[runtime]],
+		properties: {
+			'camel.version': camelVersion
+		},
+		cwd: WORKSPACE_PATH
+	});
+
+	maven.spawn();
+	return maven;
+}
+
+async function analyzeProject(maven: Maven): Promise<RuntimeOutput> {
+	const analyzer = new LogAnalyzer(maven.stdoutLineReader);
+
+	analyzer.whenMatchesThenCaptureData(/.*Total (?<totalRoutes>\d+) routes, of which (?<startedRoutes>\d+) are started/);
+	analyzer.whenMatchesThenCaptureData(
+		/.*Apache Camel (?<camelVersion>\d+\.\d+\.\d+(|\.[a-zA-Z0-9-_]+)) \(CamelContext: .+\) started in.*/
+	);
+	analyzer.startOrderedParsing();
+
+	const analyzerResult = await analyzer.wait() as RuntimeOutput;
+	return analyzerResult;
+}
+
+function getExpectedNumberOfRoutes(args: TestArguments): number {
+	switch (args.type) {
+		case 'file':
+			return 10;
+		case 'url':
+			return 2;
+	}
+}
+
+function getCamelContextPath(args: TestArguments): string {
+	switch (args.framework) {
+		case 'spring':
+			return 'src/main/resources/META-INF/spring/camel-context.xml';
+		case 'blueprint':
+			return 'src/main/resources/OSGI-INF/blueprint/blueprint.xml';
+	}
+}
+
+function getExpectedFileList(args: TestArguments): string[] {
+	let files = [
+		'wsdl2rest.readme.md',
+		'config/logging.properties',
+		getCamelContextPath(args)
+	];
+
+	let sourceRoot: string;
+	switch (args.type) {
+		case 'file':
+			sourceRoot = '/src/main/java/org/jboss/fuse/wsdl2rest/test/doclit';
+			files.push(
+				`${sourceRoot}/AddAddress.java`,
+				`${sourceRoot}/AddAddressResponse.java`,
+				`${sourceRoot}/Address.java`,
+				`${sourceRoot}/AddressService.java`,
+				`${sourceRoot}/DelAddress.java`,
+				`${sourceRoot}/DelAddressResponse.java`,
+				`${sourceRoot}/GetAddress.java`,
+				`${sourceRoot}/GetAddressResponse.java`,
+				`${sourceRoot}/Item.java`,
+				`${sourceRoot}/ListAddresses.java`,
+				`${sourceRoot}/ListAddressesResponse.java`,
+				`${sourceRoot}/ObjectFactory.java`,
+				`${sourceRoot}/package-info.java`,
+				`${sourceRoot}/UpdAddress.java`,
+				`${sourceRoot}/UpdAddressResponse.java`,
+			);
+			break;
+		case 'url':
+			sourceRoot = 'src/main/java/org/helloworld/test/rpclit';
+			files.push(
+				`${sourceRoot}/HelloPortType.java`,
+				`${sourceRoot}/HelloService.java`,
+			);
+			break;
+	}
+	return files;
+}
+
+async function getInput(): Promise<Input> {
+	await InputBox.create();
+	return Input.getInstance();
+} 
